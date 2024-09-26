2f0913fb98a1da44db80422ffab6aa93f2e1340c
Anandra Syarifudin <anandrasyarifudin@gmail.com>
2024-09-26 21:54:59 +0700

    mengubah soal 7

--- soal 7.cpp
+++ soal 7.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int main() {
-     int a, b, c;
+     int a, b, c; // deklarasi interger a, b, c
 
-    cout << "Masukkan tahun: ";
+    cout << "Masukkan tahun: "; // user masukkan tahun
     cin >> a;
 
-    cout << "Masukkan bulan: ";
+    cout << "Masukkan bulan: "; // user masukkan bulan
     cin >> b;
 
-    cout << "Masukkan hari: ";
+    cout << "Masukkan hari: "; // masukkan hari
     cin >> c;
 
-    cout << "Hasil: " << a*365 + b*12 + c;
+    cout << "Hasil: " << a*365 + b*12 + c; // kalkulasi hasil akhir
     return 0;
 }
